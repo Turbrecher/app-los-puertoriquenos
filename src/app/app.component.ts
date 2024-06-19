c600f1cedd63e492ca7032398e321af89ffac7e0
Victor Vera <victorveracoronel@live.com>
2024-06-19 23:54:47 +0200

    He creado los estilos básicos de la aplicación
    -header
    -nav
    -footer
    
    Tb. he agregado un par de rutas de prueba.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,14 @@ import { JugadoresModule } from './jugadores/jugadores.module';
 import { TorneosModule } from './torneos/torneos.module';
 import { PartidasModule } from './partidas/partidas.module';
 import { SharedModule } from './shared/shared.module';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { NavComponent } from './shared/components/nav/nav.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, JugadoresModule, TorneosModule, PartidasModule, SharedModule],
+  imports: [RouterOutlet, JugadoresModule, TorneosModule, PartidasModule, SharedModule, HeaderComponent, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,26 @@
 import { Routes } from '@angular/router';
 import { JugadoresCreateComponent } from './jugadores/components/jugadores-create/jugadores-create.component';
+import { RulesComponent } from './shared/components/rules/rules.component';
+import { TorneosListComponent } from './torneos/components/torneos-list/torneos-list.component';
+import { PartidasCreateComponent } from './partidas/components/partidas-create/partidas-create.component';
+import { TorneosCreateComponent } from './torneos/components/torneos-create/torneos-create.component';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {
-        path:"", component:JugadoresCreateComponent
-    }
+        path: "rules", component: RulesComponent
+    },
+    {
+        path: "tournaments", component: TorneosListComponent
+    },
+    {
+        path: "creategame", component: PartidasCreateComponent
+    },
+    {
+        path: "createtournament", component: TorneosCreateComponent
+    },
+    {
+        path: "**", component: PageNotFoundComponent
+    },
+    
 ];
